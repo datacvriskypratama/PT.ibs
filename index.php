bbbcfa9bcfadaee6607d9d57c3ae8dbae37ccb67
datacvriskypratama <81645433+datacvriskypratama@users.noreply.github.com>
2022-08-04 00:59:25 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PT.IBS</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+  <b>
+<span style="color:#FF0000">P</span><span style="color:#FF6600">T</span><span style="color:#FFCC00">.</span><span style="color:#CCFF00">I</span><span style="color:#66FF00">B</span><span style="color:#00FF00">S</span> <span style="color:#00FF66">T</span><span style="color:#00FFCC">R</span><span style="color:#00CCFF">A</span><span style="color:#0066FF">D</span><span style="color:#0000FF">E</span><span style="color:#6600FF">M</span><span style="color:#CC00FF">A</span><span style="color:#FF00CC">R</span><span style="color:#FF0066">K</span>
+</b>
+<br />
+<b>
+<a href="https://youtube.com/channel/UCkcWIClWldDnmefW5CUxHIg"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmtsAmDCEw7ChtoiezWgADmapEA8k2CUqqlw&usqp=CAU" alt="ibs profik" / width="15%"></a>
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9849417033161051"
+     crossorigin="anonymous"></script>
+     
+</body>
+</html>
