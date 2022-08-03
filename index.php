f7c4f3ccb985fc6a84da4102a992fc850338d9f4
datacvriskypratama <81645433+datacvriskypratama@users.noreply.github.com>
2022-08-04 01:04:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
 </b>
 <br />
 <b>
+<span style="color:#FF0000">H</span><span style="color:#FF3D00">A</span><span style="color:#FF7A00">K</span> <span style="color:#FFB800">C</span><span style="color:#FFF500">I</span><span style="color:#CCFF00">P</span><span style="color:#8FFF00">T</span><span style="color:#52FF00">A</span> <span style="color:#14FF00">B</span><span style="color:#00FF29">Y</span> <span style="color:#00FF66">P</span><span style="color:#00FFA3">T</span><span style="color:#00FFE0">.</span><span style="color:#00E0FF">I</span><span style="color:#00A3FF">B</span><span style="color:#0066FF">S</span> <span style="color:#0029FF">T</span><span style="color:#1400FF">R</span><span style="color:#5200FF">A</span><span style="color:#8F00FF">D</span><span style="color:#CC00FF">E</span><span style="color:#FF00F5">M</span><span style="color:#FF00B8">A</span><span style="color:#FF007A">R</span><span style="color:#FF003D">K</span>
+</b>
+<br />
 <a href="https://youtube.com/channel/UCkcWIClWldDnmefW5CUxHIg"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmtsAmDCEw7ChtoiezWgADmapEA8k2CUqqlw&usqp=CAU" alt="ibs profik" / width="15%"></a>
 
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9849417033161051"
